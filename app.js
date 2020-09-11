e9097147d5ec0159f7e4a33e7ee7e57b356cc4f6
joticd <joticd@gmail.com>
2020-09-11 11:57:16 +0200

    rescale

--- app.js
+++ app.js
@@ -3,13 +3,34 @@ const sharp = require('sharp');
 const {whatToDo, symbolAnimation, rescale} = require('./config/config.json');
 
 const assetsPath = `${__dirname}/assets`;
-const files = fs.readdirSync(assetsPath);
-
-const prefixOriginal = symbolAnimation.original;
-const prefixReplace = symbolAnimation.replace;
+const files = fs.readdirSync(assetsPath);	
 
 files.forEach(element =>{    
-    // let replaceName = element.replace(new RegExp(prefixOriginal), prefixReplace )
-    // fs.renameSync(`${assetsPath}/${element}`, `${assetsPath}/${replaceName}`);
-    
-})
+    if(whatToDo.rename){
+        renameSymFun(element);
+    }
+    if(whatToDo.rescale){
+        rescaleSymFun(element);
+    }
+});
+
+function renameSymFun (element){
+    const prefixOriginal = symbolAnimation.original;
+    const prefixReplace = symbolAnimation.replace;
+    let replaceName = element.replace(new RegExp(prefixOriginal), prefixReplace );
+    fs.renameSync(`${assetsPath}/${element}`, `${assetsPath}/${replaceName}`);
+};
+
+function rescaleSymFun (element){  
+    sharp(`${assetsPath}/${element}`)
+    .resize({height : rescale.height})
+    .toBuffer()
+    .then( data => {
+        fs.writeFileSync(`${assetsPath}/${element}`, data);
+    })
+    .catch( err => {
+        console.log(err);
+    });
+}
+
+
