d108f30515d1dd1257b380b27b50de6c58d308cd
joticd <joticd@gmail.com>
2020-09-11 08:48:36 +0200

    delete assets

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const sharp = require('sharp');
 const {whatToDo, symbolAnimation, rescale} = require('./config/config.json');
 
 const assetsPath = `${__dirname}/assets`;
@@ -8,7 +9,7 @@ const prefixOriginal = symbolAnimation.original;
 const prefixReplace = symbolAnimation.replace;
 
 files.forEach(element =>{    
-    let replaceName = element.replace(new RegExp(prefixOriginal), prefixReplace )
-    fs.renameSync(`${assetsPath}/${element}`, `${assetsPath}/${replaceName}`);
+    // let replaceName = element.replace(new RegExp(prefixOriginal), prefixReplace )
+    // fs.renameSync(`${assetsPath}/${element}`, `${assetsPath}/${replaceName}`);
     
 })
